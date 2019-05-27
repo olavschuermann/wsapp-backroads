faff851d90a9397e1148b5ea73d2f0e9b20d71c5
Olav Schuermann <developer@netzpluslogik.de>
2019-05-27 18:09:03 +0200

    planning styling of webapp

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Layout from '../components/Layout';
 const App = () => {
     return (    
         <Layout>
-                Hello world (Website-App React/Gatsby 0.0.4 - 26.05.19)<br />
+                Hello world (Website-App React/Gatsby 0.0.5 - 27.05.19)<br />
                 <Link to='/blog'>Blog</Link>
         </Layout>
     )}
