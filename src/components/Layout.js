7afd329e35fe32cfdba03c1f2a3abc80701ed870
Olav Schuermann <developer@netzpluslogik.de>
2019-06-07 08:30:42 +0200

    adding styled footer to website

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,7 +9,7 @@ const Layout = ({ children }) => {
     return (
         <div>
             <NavBar />
-            {children}
+                {children}
             <Footer />
         </div>
     )
